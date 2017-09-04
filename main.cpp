4498190129b9426d818c46b03b626581394adbce
Peter Buelow <peter.buelow@lifefitness.com>
2017-09-04 15:04:41 -0400

    Making it do something now

--- main.cpp
+++ main.cpp
@@ -5,11 +5,92 @@
  *      Author: pete
  */
 
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <wiriginPi.h>
 #include "DS18B20.h"
 #include "PocketGeiger.h"
 #include "tsl2561.h"
 
-int main(int argc, char *argv[])
+void *tsl;
+DS18B20 ds18b20_1;
+DS18B20 ds18b20_2;
+
+void onRadiation()
+{
+	std::cout << "Wild Gamma Rays are a happenin: " << PocketGeiger::instance().uSvh();
+	std::cout << "uSv/h +/- " << PocketGeiger::instance().uSvhError();
+}
+
+void onNoise()
+{
+	std::cout << "too much noise";
+}
+
+PI_THREAD (luxThread)
+{
+	int c;
+	long lux;
+
+	if (tsl != NULL) {
+		while (1) {
+			lux = tsl2561_lux(tsl);
+			std::cout << "lux: " << lux;
+			usleep(3 * 1000 * 1000);
+		}
+	}
+}
+
+PI_THREAD (tempThread)
+{
+	ds18b20_1.open("sdlk");
+	ds18b20_2.open("lksd");
+
+	while (1) {
+		ds18b20_1.read();
+		ds18b20_2.read();
+		std::cout << __PRETTY_FUNCTION__ << ": temp=" << ds18b20_1.tempF();
+		std::cout << __PRETTY_FUNCTION__ << ": temp=" << ds18b20_2.tempF();
+		usleep(1000 * 1000);
+	}
+}
+
+void runtime()
 {
 
+	piThreadCreate(luxThread);
+
+	while (1) {
+		sleep(1);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+
+	if ((tsl = tsl2561_init(0x39, "/dev/i2c-1")) == NULL) {
+		std::cerr << "Unable to open TSL device";
+	}
+
+	PocketGeiger::instance().registerRadiationCallback(&onRadiation);
+	PocketGeiger::instance().registerNoiseCallback(&onRadiation);
+
+	tsl2561_enable_autogain(tsl);
+	tsl2561_set_integration_time(tsl, TSL2561_INTEGRATION_TIME_13MS);
+
+	pid = fork();
+	if (pid == 0) {
+		runtime();
+	}
+	else if (pid < 0) {
+		std::cerr << "fork failed";
+	}
+	else {
+		fclose(stdout);
+		fclose(stdin);
+		fclose(stderr);
+	}
+	exit(0);
 }
